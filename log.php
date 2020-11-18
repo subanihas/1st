9247fd3aabba2d5adfc6c13f586bb56a05fccd47
subanihas <74642323+subanihas@users.noreply.github.com>
2020-11-18 11:28:55 +0530

    Add files via upload

---
+++ log.php
@@ -0,0 +1,41 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.container {
+  position: relative;
+  font-family: Script MT Bold;
+}
+
+.text-block {
+  position: absolute;
+  top: 100px;
+  center: 50px;
+  text-align:center;
+  background-color: none;
+  color: black;
+  padding-left: 100px;
+  padding-right: 600px;
+}
+</style>
+</head>
+<body>
+
+<div class="container" >
+  <img src="http://localhost/my project/me.jpg" width="1500" height="800">
+  <div class="text-block">
+     <h1>  <br><br>  &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp ADMIN</h1>
+<h2>
+<form method="post" action="log1.php">
+USERNAME&nbsp &nbsp <input type="text" name="usn"><br><br>
+PASSWORD&nbsp &nbsp <input type="password" name="pas"><br><br>
+<input type="submit" value="LOGIN" name="login" style="font-weight:bold;color:black;background-color:white;font-size:20;"></h2>
+</form>
+  </div>
+</div>
+
+</body>
+</html> 
